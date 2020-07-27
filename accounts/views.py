5be6166601118c4d7fd9e45d61cb2a28254b7e43
LeeYeJin <kains0417@naver.com>
2020-07-27 15:39:36 +0900

    05 회원가입 구현하기

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,31 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
+from django.shortcuts import redirect, render
+from .forms import SignupForm
+
+'''
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = SignupForm()
+    return render(request, 'accounts/signup.html',{
+        'form': form,
+    })
+'''
+
+signup = CreateView.as_view(model=User, 
+        form_class=SignupForm,
+        success_url=settings.LOGIN_URL,
+        template_name="accounts/signup.html")
+
+
 
 @login_required
 def profile(request):
