5f73d25db7e4167c3db3f94fe145faf97203dfeb
LeeYeJin <kains0417@naver.com>
2020-07-27 16:52:17 +0900

    07 회원가입과 동시에 로그인하기

--- accounts/views.py
+++ accounts/views.py
@@ -1,29 +1,42 @@
 from django.conf import settings
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.views.generic import CreateView
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, resolve_url
 from .forms import SignupForm
 
-'''
-def signup(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            return redirect(settings.LOGIN_URL)
-    else:
-        form = SignupForm()
-    return render(request, 'accounts/signup.html',{
-        'form': form,
-    })
-'''
 
-signup = CreateView.as_view(model=User, 
-        form_class=SignupForm,
-        success_url=settings.LOGIN_URL,
-        template_name="accounts/signup.html")
+# def signup(request):
+#     if request.method == 'POST':
+#         form = SignupForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             # 로그인 처리 
+#             auth_login(request, user)
+#             return redirect('profile')
+#     else:
+#         form = SignupForm()
+#     return render(request, 'accounts/signup.html',{
+#         'form': form,
+#     })
+
+
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        return resolve_url('profile')
+
+    def form_valid(self , form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect(self.get_success_url())
+
+signup = SignupView.as_view()
 
 
 
