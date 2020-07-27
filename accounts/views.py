e7ba2426d5bda32503e00162c4b105cedfc31e44
LeeYeJin <kains0417@naver.com>
2020-07-27 18:43:46 +0900

    08 회원가입 시에 next인자 처리하기

--- accounts/views.py
+++ accounts/views.py
@@ -12,10 +12,10 @@ from .forms import SignupForm
 #     if request.method == 'POST':
 #         form = SignupForm(request.POST)
 #         if form.is_valid():
-#             user = form.save()
-#             # 로그인 처리 
+#             user = form.save() # 로그인 처리 
 #             auth_login(request, user)
-#             return redirect('profile')
+#             next_url = request.GET.get('next') or 'profile'
+#             return redirect(next_url)
 #     else:
 #         form = SignupForm()
 #     return render(request, 'accounts/signup.html',{
@@ -29,9 +29,10 @@ class SignupView(CreateView):
     template_name = 'accounts/signup.html'
 
     def get_success_url(self):
-        return resolve_url('profile')
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
 
-    def form_valid(self , form):
+    def form_valid(self, form):
         user = form.save()
         auth_login(self.request, user)
         return redirect(self.get_success_url())
