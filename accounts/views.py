ca4017fdc0d0f9935b14b6ee8b201a16edaa80c7
LeeYeJin <kains0417@naver.com>
2020-07-27 14:13:37 +0900

    03 프로필 페이지 구현하기

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+@login_required
+def profile(request):
+    request.user # django.contrib,auth.models.AnonymousUser
+    return render(request, 'accounts/profile.html')
